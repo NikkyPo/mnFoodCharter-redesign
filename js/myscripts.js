248eee4aa21ad7a2b0616f5ba2ee8e9f9b0b9b0b
NikkyPo <nihelg@gmail.com>
2018-04-30 17:35:29 -0500

    code that adds map and work on other tabs

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 
 	var champions = [
-	"<h2><strong>NADJA BERNECHE:</strong><h2> <br> HEALTHY COMPREHENSIVE PLAN DIRECTOR, <br> TERRA SOMA FOOD INFRASTRUCTURE", 
-	"<h2><strong>AMANDA SHONGO:<strong><h2> <br> STATEWIDE HEALTH IMPROVEMENT PROGRAMS (SHIP) COORDINATOR, <br> LEECH LAKE BAND OF OJIBWE FOOD ACCESSIBILITY", 
-	"<h2><strong>AMANDA SHONGO:</strong><h2> <br> STATEWIDE HEALTH IMPROVEMENT PROGRAMS (SHIP) COORDINATOR, <br> LEECH LAKE BAND OF OJIBWE FOOD ACCESSIBILITY", 
-	"<h2><strong>REGINALDO HASLETT-MARROQUIN:</strong><h2> <br> CHIEF STRATEGY OFFICER, <br> MAIN STREET PROJECT FOOD INFRASTRUCTURE"
+	"<h3><strong>NADJA BERNECHE:</strong><h3><h5>HEALTHY COMPREHENSIVE PLAN DIRECTOR, <br> TERRA SOMA FOOD INFRASTRUCTURE</h5><h6>'This is fun and important work, because policy affects all lives, and I love to see people understand policy processes, find their voices, and get excited about policy change'</h6>", 
+	"<h3><strong>AMANDA SHONGO:<strong><h3><h5> STATEWIDE HEALTH IMPROVEMENT PROGRAMS (SHIP) COORDINATOR, <br> LEECH LAKE BAND OF OJIBWE FOOD ACCESSIBILITY<h5><h6>'My passion for food access, sustainable food systems, and food sovereignty stems from my personal history growing up in extreme poverty'</h6>", 
+	"<h3><strong>MARY JO FORBOD:</strong><h3><h5> COORDINATOR, MORRIS HEALTHY EATING, <br> UNIVERSITY OF MINNESOTA, MORRIS</h5><h6>'We want to make healthy foods accessible and affordable to everyone in our community, regardless of income, age, or where they live'</h6>", 
+	"<h3><strong>REGINALDO HASLETT-MARROQUIN:</strong><h3><h5> CHIEF STRATEGY OFFICER, <br> MAIN STREET PROJECT FOOD INFRASTRUCTURE<h5><h6>'Food is not just about feeding people. It’s about ensuring folks take ownership and control of systems and infrastructure'</h6>"
 	]; //create an array
 	var images = ["champion1.jpg", "champion2.jpg", "champion3.jpg", "champion4.jpg"];   //results are stored in different variable
 
